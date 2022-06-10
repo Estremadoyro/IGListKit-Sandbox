91984764a6d2d510a7c4194f6c223289a5ec6b99
Leonardo <lmestremadoyro@gmail.com>
2022-06-09 21:32:23 -0500

    initial project

---
+++ IGListKit Sandbox/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IGListKit Sandbox
+//
+//  Created by Leonardo  on 9/06/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.systemPink
+  }
+}
